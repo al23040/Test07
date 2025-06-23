5933800a071d88cb519992188c75bb4ae2899f89
al23040 <al23040@shibaura-it.ac.jp>
2025-06-23 22:39:16 +0900

    C3 commit TranscriptReader.py

--- backend/C3/TranscriptReader.py
+++ backend/C3/TranscriptReader.py
@@ -1,11 +1,18 @@
+from typing import Optional
+
 import pdfplumber
 from io import BytesIO
 import logging
+
 logging.getLogger("pdfminer").setLevel(logging.ERROR)
 import re
 import utils
 
+
 class TranscriptReader:
+    def __init__(self):
+        self.term: Optional[dict] = None
+
     def is_transcript(self, pdf_data: bytes) -> bool:
         keyword = "芝浦工業大学"
         with pdfplumber.open(BytesIO(pdf_data)) as pdf:
@@ -13,9 +20,27 @@ class TranscriptReader:
             words = page.extract_words()
             for word in words:
                 if word['text'] == keyword and word['x0'] < 490 and word['top'] < 35:
+                    # 成績通知表より現在の年次を取得
+                    termpattern = re.compile(r"(20\d{2})年度\s*(前期|後期)")
+                    found_terms: list[Tuple[int, str]] = []
+                    for p in pdf.pages:
+                        text = p.extract_text()
+                        if text:
+                            for match in termpattern.finditer(text):
+                                year = int(match.group(1))
+                                semester = match.group(2)
+                                found_terms.append((year, semester))
+
+                    if found_terms:
+                        def sort_key(term):
+                            return term[0], 1 if term[1] == "前期" else 2
+
+                        latest = max(found_terms, key=sort_key)
+                        self.term = {"year": latest[0], "semester": latest[1]}
+
                     return True
         return False
-    
+
     def get_course_data(self, pdf_data: bytes):
         courses = []
         pattern = re.compile(r'(.+?)\s+([A-Z0-9]{7,})\s+(\d+)\s+(\d)\s+([SABCDGF#])\s+(\d)\s+\d{2}')
@@ -36,13 +61,13 @@ class TranscriptReader:
                     code = match.group(2)
                     grade = match.group(5)
                     courses.append({
-                        "科目名": subject_name,
-                        "科目コード": code,
-                        "成績": grade
+                        "subject_name": subject_name,
+                        "code": code,
+                        "grade": grade
                     })
 
-            utils.make_send_data(courses)
+            send_courses = utils.make_send_courses(courses)
+            send_available_courses = utils.make_send_available_courses(self.term, send_courses)
+            make_send_credits_data = utils.make_send_credits_data(send_courses)
 
 
-
-            return  courses
